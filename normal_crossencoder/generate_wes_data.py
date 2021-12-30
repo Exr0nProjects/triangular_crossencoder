cf77f0550b961ebdd539e07087aa25840958dbee
Exr0n <mail@exr0n.com>
2021-12-29 18:44:49 -0800

    generate positive and negative pairs from WES

---
+++ normal_crossencoder/generate_wes_data.py
@@ -0,0 +1,67 @@
+from datasets import load_dataset
+import numpy as np
+from tqdm import tqdm
+
+from collections import defaultdict
+from operator import itemgetter as ig
+from itertools import islice, chain, repeat
+from random import sample, choice
+from gc import collect
+
+SUBSET = '2018thresh20'
+
+def generate_splits(split=[0.75, 0.15, 0.1], subset=SUBSET):
+    assert abs(sum(split) - 1.0) < 0.0001
+    # get the data in dictionary form
+    groups = defaultdict(list)
+    ds = load_dataset('Exr0n/wiki-entity-similarity', subset, split='train')
+    ds = list(tqdm(islice(ds, int(1e4)), total=len(ds)))
+    for article, link in tqdm(map(ig('article', 'link_text'), ds), total=len(ds)):
+        groups[article].append(link)
+    del ds
+
+    # greedily allocate splits
+    order = sorted(groups.keys(), reverse=True, key=lambda e: groups[e])
+    splits = [[] for _ in split]
+    sizes = [0.001] * len(split)    # avoid div zero error
+    for group in order:
+        impoverished = np.argmax([ s - (x/sum(sizes)) for x, s in zip(sizes, split) ])
+        splits[impoverished].append(group)
+        sizes[impoverished] += len(groups[group])
+
+    sizes = [ int(x) for x in sizes ]
+    print('final sizes', sizes, [x/sum(sizes) for x in sizes])
+
+    # generate positive examples
+    ret = [ [[(k, t) for t in groups[k]] for k in keys] for keys in splits ]
+
+    # generate negative examples randomly (TODO: probably a more elegant swapping soln)
+    for i, keys in enumerate(splits):
+        for key in keys:
+            try:
+                got = sample(keys, len(groups[key])+1)
+                ret[i].append(
+                    [(key, choice(groups[k])) for k in got if k != key]
+                    [:len(groups[key])]
+                )
+            except ValueError:
+                raise ValueError("well frick one group is bigger than all the others combined. try sampling one at a time")
+
+    collect()
+    return [(chain(*s), chain(repeat(0, z), repeat(1, z))) for z, s in zip(sizes, ret)]
+
+
+if __name__ == '__main__':
+    x = generate_splits([0.5, 0.4, 0.1])
+    for data, labels in x[2:]:
+        data = list(data)
+        labels = list(labels)
+
+        assert sum(labels) * 2 == len(labels)
+        num = sum(labels)
+
+        before = [ a for a, _ in data[:num] ]
+        after  = [ a for a, _ in data[num:] ]
+        assert before == after
+
+        print(data[num:])
